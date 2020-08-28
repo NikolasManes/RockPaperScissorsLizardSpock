93af40bd9789f5de35b710911ef271a7ccba37f3
Nikolas <std128185@ac.eap.gr>
2020-08-28 12:15:03 +0300

    try another way for the animation

--- app/src/main/java/com/nikolas/rock_paper_scissors_lizard_spock/GameActivity.java
+++ app/src/main/java/com/nikolas/rock_paper_scissors_lizard_spock/GameActivity.java
@@ -1,10 +1,18 @@
 package com.nikolas.rock_paper_scissors_lizard_spock;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Path;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Scene;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private enum GameResult {WIN, LOSE, DRAW}
     private enum PlayerAction {ROCK, PAPER, SCISSORS, LIZARD, SPOCK}
@@ -12,38 +20,128 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private PlayerAction humanAction;
     private PlayerAction computerAction;
     private GameResult result;
-    private String toastResult;
     private String toastInfo;
+
+    private ImageView rockViewComputer;
+    private ImageView paperViewComputer;
+    private ImageView scissorsViewComputer;
+    private ImageView lizardViewComputer;
+    private ImageView spockViewComputer;
+
+    private ImageView rockViewHuman;
+    private ImageView paperViewHuman;
+    private ImageView scissorsViewHuman;
+    private ImageView lizardViewHuman;
+    private ImageView spockViewHuman;
+
+    private ImageView prevView;
+    private ImageView nextView;
+
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Initialize "buttons" etc
+        viewFlipper = findViewById(R.id.view_flipper);
+
+        rockViewComputer = findViewById(R.id.rock_img_1);
+        paperViewComputer = findViewById(R.id.paper_img_1);
+        scissorsViewComputer = findViewById(R.id.scissors_img_1);
+        lizardViewComputer = findViewById(R.id.lizard_img_1);
+        spockViewComputer = findViewById(R.id.spock_img_1);
+
+        rockViewHuman = findViewById(R.id.rock_img_2);
+        paperViewHuman = findViewById(R.id.paper_img_2);
+        scissorsViewHuman = findViewById(R.id.scissors_img_2);
+        lizardViewHuman = findViewById(R.id.lizard_img_2);
+        spockViewHuman = findViewById(R.id.spock_img_2);
+
+        prevView = findViewById(R.id.prev_button);
+        nextView = findViewById(R.id.next_button);
+
+        rockViewHuman.setOnClickListener(this);
+        paperViewHuman.setOnClickListener(this);
+        scissorsViewHuman.setOnClickListener(this);
+        lizardViewHuman.setOnClickListener(this);
+        spockViewHuman.setOnClickListener(this);
+
+        prevView.setOnClickListener(this);
+        nextView.setOnClickListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.prev_button:
+                viewFlipper.showPrevious();
                 break;
             case R.id.next_button:
+                viewFlipper.showNext();
                 break;
             case R.id.rock_img_2:
                 humanAction = PlayerAction.ROCK;
+                rockViewHuman.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerChoice();
                 break;
             case R.id.paper_img_2:
                 humanAction = PlayerAction.PAPER;
+                paperViewHuman.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerChoice();
                 break;
             case R.id.scissors_img_2:
                 humanAction = PlayerAction.SCISSORS;
+                scissorsViewHuman.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerChoice();
                 break;
             case R.id.lizard_img_2:
                 humanAction = PlayerAction.LIZARD;
+                lizardViewHuman.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerChoice();
                 break;
             case R.id.spock_img_2:
                 humanAction = PlayerAction.SPOCK;
+                spockViewHuman.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerChoice();
                 break;
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void runAnimation(){
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void computerChoice() {
+        Random random = new Random();
+        runAnimation();
+        switch (random.nextInt(5)+1){
+            case 1:
+                rockViewComputer.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerAction = PlayerAction.ROCK;
+                break;
+            case 2:
+                paperViewComputer.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerAction = PlayerAction.PAPER;
+                break;
+            case 3:
+                scissorsViewComputer.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerAction = PlayerAction.SCISSORS;
+                break;
+            case 4:
+                lizardViewComputer.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerAction = PlayerAction.LIZARD;
+                break;
+            case 5:
+                spockViewComputer.setBackground(getDrawable(R.drawable.circle_cyan));
+                computerAction = PlayerAction.SPOCK;
+                break;
+        }
+
     }
 
     private void setResult(){
